28395bfa088afa53712e9edeb0c4476d4d8678ed
chabibnr <chabibnurozak@gmail.com>
2013-04-10 17:39:31 +0700

    Update Password DB

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('EXT','.php');
 define('THEME_USE','default');
 define('SITEURL','http://localhost/chabibcms/');
 define('DB_HOST','localhost');
-define('DB_PASSWORD','Admin');
+define('DB_PASSWORD','');
 define('DB_USER','root');
 define('DB_DATABASE','chabibcms');
 define('PREFIX_TABLE','ch_');
