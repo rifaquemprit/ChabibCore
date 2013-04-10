951ce7f153857e8d0ba5fea3e181a32aa3b8b051
chabibnr <chabibnurozak@gmail.com>
2013-04-10 22:45:21 +0700

    Create example config menu in module

---
+++ admin/module/carts/config.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+Menu::newsub('Product List','?m=carts/pl','',2);
+menu::create('Shopping','?m=carts','',2);
+?>
