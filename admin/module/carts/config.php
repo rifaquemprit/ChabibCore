74267d862ca788e44131c2c8927ca01ca338316d
Chabib Nurozak <chabibnurozak@gmail.com>
2013-04-13 11:37:11 +0700

    Update config.php

--- admin/module/carts/config.php
+++ admin/module/carts/config.php
@@ -4,6 +4,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-Menu::newsub('Product List','?m=carts/pl','',2);
-menu::create('Shopping','?m=carts','',2);
+Menu::newsub('Product List','carts/pl.ch','',2);
+menu::create('Shopping','carts.ch','',2);
 ?>
